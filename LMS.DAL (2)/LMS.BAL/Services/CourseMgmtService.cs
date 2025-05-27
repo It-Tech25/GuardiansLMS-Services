c77a9da24bfefa60bfb685d727d7d9f14789ad6d
kumar <kumar@DESKTOP-1NOFRFJ>
2025-05-27 16:47:10 +0530

    changes

--- LMS.DAL (2)/LMS.BAL/Services/CourseMgmtService.cs
+++ LMS.DAL (2)/LMS.BAL/Services/CourseMgmtService.cs
@@ -45,7 +45,7 @@ namespace LMS.BAL.Services
             if (req.CourseId == 0)
             {
                 m.CourseName = req.CourseName;
-                m.ContactNumber = req.CourseName; 
+                m.ContactNumber = req.ContactNumber; 
                 m.Description = req.Description;
                 m.CourseImgURL = req.CourseImgURL;
                 m.CourseCode = req.CourseCode; 
